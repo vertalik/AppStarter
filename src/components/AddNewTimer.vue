0b4edd0ba5fdbc02318763f8f4a31342a8a91233
vertalik <kolumb.vk@gmail.com>
2020-05-31 15:36:39 +0300

    Added responcive version

--- src/components/AddNewTimer.vue
+++ src/components/AddNewTimer.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
+  <div class="app-timer__add">
     <input type="text" class="timer-name__input" v-model="newTimerName" placeholder="Timer name" @keyup.enter="addTimer">
     <input type="button" class="add-new-timer__btn" value="Create Timer" @click="addTimer">
+     <div class="horizont-line"></div>
   </div>
 </template>
 
@@ -21,6 +22,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.app-timer__add {
+  padding-top: 40px;
+  position: sticky;
+  top:0;
+  background-color: #fff;
+}
+
+.horizont-line {
+  height: 1px;
+  background: #E7E8EA;
+}
+
 .timer-name__input {
  padding: 14px 0 14px 20px;;
  width: 305px;
@@ -33,10 +46,25 @@ export default {
 
 .add-new-timer__btn{
   padding: 14px 25px;
+  font-family: 'Nunito Sans Bold', sans-serif;
+  letter-spacing: 1px;
   background: linear-gradient(163.14deg, #FF8E64 0%, #FFE641 100%);
   border-radius: 6px;
   border-style: none;
   color: #fff;
   margin-bottom: 30px;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 550px) {
+  .app-timer__add {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .timer-name__input {
+    width: 100%;
+    margin-bottom: 20px;
+  }
 }
 </style>
--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -8,13 +8,19 @@
   </td>
   <td class="timer__colum">
     <button class="trigger" @click="isRun = !isRun">
-    <img src="../assets/img/pause_btn.png" v-if="isRun" alt="">
-    <img src="../assets/img/play_btn.png" v-else alt="">
+      <div class="pause__btn" v-if="isRun">
+        <span class="mdi mdi-pause"></span>
+      </div>
+      <div class="run__btn" v-else>
+       <span class="mdi mdi-play"></span> 
+      </div>
   </button>
   </td>
   <td class="timer__colum">
-    <button class="trigger" @click="onDelete(id)">
-     <img src="../assets/img/delete_btn.png" alt="">
+    <button class="trigger del__btn" @click="onDelete(id)">
+     <div class="del__btn">
+       <span class="mdi mdi-delete"></span> 
+      </div>
   </button>
   </td>
 </div>
@@ -102,9 +108,37 @@ export default {
   background-color: inherit;
   border-style: none;
   outline:none;
+  width: 50px;
+  height: 50px;
   & :hover {
     cursor: pointer;
   }
+}
+
+.run__btn {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  background: linear-gradient(135deg, #009FC5 0%, #3CECB0 100%);
+  border-radius: 50%;
+    span {
+      font-size: 1.5rem;
+      color: #fff;
+   }
+}
+
+.pause__btn {
+  @extend .run__btn;
+  background: linear-gradient(135deg, #7956EC 0%, #2FB9F8 100%);
+}
+
+.del__btn{
+  @extend .run__btn;
+  background: linear-gradient(135deg, #F23673 0%, #FCA069 100%);
+  border-radius: 6px;
+
 }
 
 .timer__container {
@@ -115,6 +149,9 @@ export default {
 
 .timer-name{
   color: #5586F2;
+  font-family: 'Nunito Sans Bold', sans-serif;
+  font-size: 1.25rem;
+  overflow: hidden;
 }
 
 .timer__colum:first-child {
@@ -158,6 +195,13 @@ export default {
 
 .disabled__timer {
   background-color: #E7E8EA;
+}
+
+@media screen and (max-width: 550px) {
+  .timer__colum:first-child, .timer__colum:nth-child(3), .timer__colum:nth-child(2) {
+  flex-basis: 25%;
+  margin: 0;
+  }
 }
 
 </style>
