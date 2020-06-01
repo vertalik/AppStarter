fc2ac90d7ea128088cf47fe50ddb38979097061a
vertalik <kolumb.vk@gmail.com>
2020-06-01 11:33:11 +0300

    local change

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import './assets/style/main.scss'
 import Vue from 'vue'
 import App from './App.vue'
+import Timer from './components/Timer.vue';
+import AddNewTimer from './components/AddNewTimer.vue';
 import moment from 'moment';
 
 Vue.prototype.$moment = moment;
@@ -9,4 +11,6 @@ Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  Timer,
+  AddNewTimer
 }).$mount('#app')
